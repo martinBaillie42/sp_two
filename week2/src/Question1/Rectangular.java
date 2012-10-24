b36f1b419e4f8c014a2985f63931217dc333f1c8
Martin Baillie <martin@martinbaillie.net>
2012-10-24 22:35:25 +0100

    Question 1 and 2

--- week2/src/Question1/Rectangular.java
+++ week2/src/Question1/Rectangular.java
@@ -7,7 +7,17 @@ public class Rectangular {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int noOfBlocks = 20;
+		int width = 5;
+		int height = noOfBlocks / width;
+		
+		for (int i = 1; i <= height; i++){
+			for (int j = 1; j <= width; j++){
+				System.out.print("x ");
+			}
+			System.out.println();	
+		}
+				
 	}
 
 }
--- week2/src/Question2/Ramp.java
+++ week2/src/Question2/Ramp.java
@@ -7,6 +7,21 @@ public class Ramp {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int noOfBlocks = 15;
+		int width = noOfBlocks / 3;
+		int height = noOfBlocks / 3;
+		
+		for (int i = 0; i < height; i++){
+			if (i > 0){
+				for (int k = 1; k <= i; k++){
+					System.out.print("  ");
+				}
+			}
+			for (int j = 0; j < width - i; j++){
+				System.out.print("x ");
+			}
+			System.out.println();
+		}
 
 	}
 
