befc296b5dd3e73b7715c80d82e9de14ac929b54
Martin Baillie <martin@martinbaillie.net>
2012-10-28 20:04:37 +0000

    Question 16

---
+++ week2/src/question17/Converter.java
@@ -0,0 +1,17 @@
+package question17;
+
+public class Converter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte aByte = (byte) 3;
+		System.out.print(aByte);
+	}
+
+	static void bitsToHex () {
+		
+	}
+}
